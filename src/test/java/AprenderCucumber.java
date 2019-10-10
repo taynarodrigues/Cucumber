4b7f3495f25db451df4c69e7155b78fbd7f26218
Tayna Rodrigues <taynajesus@gmail.com>
2019-10-10 14:28:14 -0400

    otimizando código com o uso do Transform

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -6,43 +6,44 @@ import java.util.Date;
 
 import org.junit.Assert;
 
-import io.cucumber.java.pt.Dado;
-import io.cucumber.java.pt.Então;
-import io.cucumber.java.pt.Quando;
+import cucumber.api.Transform;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
 	
 
 	
-	@Dado("que criei o arquivo corretamente")
-	public void queCrieiOArquivoCorretamente() {
+	@Dado("^que criei o arquivo corretamente$")
+	public void queCrieiOArquivoCorretamente()  throws Throwable{
 	    System.out.println("qualquer coisa");
 	}
 
-	@Quando("executá-lo")
-	public void executáLo() {
+	@Quando("^executá-lo$")
+	public void executáLo()  throws Throwable {
 	  
 	}
 
 	
-	@Então("a especificação deve finalizar com sucesso")
+	@Então("^a especificação deve finalizar com sucesso$")
 	public void aEspecificaçãoDeveFinalizarComSucesso() {
 
 	}
 	
 	private int contador = 0;
 	
-	@Dado("que o valor do contador é {int}")
+	@Dado("^que o valor do contador é (\\d+)$")
 	public void que_o_valor_do_contador_é(int arg1) throws Throwable {
 		   contador = arg1;
 	}
 
-	@Quando("eu incrementar em {int}")
+	@Quando("^eu incrementar em (\\d+)$")
 	public void eu_incrementar_em(int arg1) throws Throwable {
 	   contador = contador + arg1;
 	}
 
-	@Então("o valor do contador será {int}")
+	@Então("^o valor do contador será (\\d+)$")
 	public void o_valor_do_contador_será( int arg1)  throws Throwable {
 
 	    Assert.assertEquals(arg1, contador);
@@ -51,13 +52,11 @@ public class AprenderCucumber {
 	
 	Date entrega = new Date();
 	
-	@Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
-	public void que_a_entrega_é_dia(int dia, int mes, int ano) throws Throwable {
-	    Calendar cal = Calendar.getInstance();
-	    cal.set(Calendar.DAY_OF_MONTH, dia);
-	    cal.set(Calendar.MONTH, mes - 1);
-	    cal.set(Calendar.YEAR, ano);
-	    entrega = cal.getTime();
+	@Dado("^que a entrega é dia (.*)$")
+	public void que_a_entrega_é_dia(@Transform(DateConverter.class) Date data) throws Throwable {
+		entrega = data;
+		System.out.println(entrega);
+	   
 	}
 
 	@Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
@@ -86,27 +85,28 @@ public class AprenderCucumber {
 	    
 	}
 
-	@Dado("^que o valor da passagem é R/\\$ (\\d+),(\\d-)$")
-	public void que_o_valor_da_passagem_é_R$(int arg1, int arg2) throws Throwable {
+	@Dado("^que o valor da passagem é  R\\$ (.*)$")
+	public void que_o_valor_da_passagem_é_R$(Double numero) throws Throwable {
+		System.out.println(numero);
 	    
 	}
 
-	@Dado("que o nome do passageiro é \"(.{5,20})\"$")
+	@Dado("^que o nome do passageiro é \"(.{5,20})\"$")
 	public void que_o_nome_do_passageiro_é(String string) throws Throwable {
 	    
 	}
 
-	@Dado("que o telefone do passageiro é (9\\d{3}-\\d{4})$")
+	@Dado("^que o telefone do passageiro é (9\\d{3}-\\d{4})$")
 	public void que_o_telefone_do_passageiro_é(String telefone) throws Throwable{
 	   
 	}
 
-	@Quando("criar steps")
+	@Quando("^criar steps$")
 	public void criar_steps() throws Throwable{
 	   
 	}
 
-	@Então("o teste vai funcionar")
+	@Então("^o teste vai funcionar$")
 	public void o_teste_vai_funcionar() throws Throwable{
 	    
 	}
--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -1,3 +1,5 @@
+
+
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
